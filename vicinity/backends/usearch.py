8c926a0ef38a6f2489d7ee37722a52bf71febdbc
Pringled <thomas123@live.nl>
2024-11-18 18:35:04 +0100

    Updates

--- vicinity/backends/usearch.py
+++ vicinity/backends/usearch.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Literal, Sequence
+from typing import Any, Literal
 
 import numpy as np
 from numpy import typing as npt
